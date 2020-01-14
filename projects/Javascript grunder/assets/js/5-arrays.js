c8548ceaabb5a900868d608444e427b0b879d290
Viktor Ek <viktek@lycksele.se>
2020-01-14 14:00:08 +0100

    continued with arrays

--- projects/Javascript grunder/assets/js/5-arrays.js
+++ projects/Javascript grunder/assets/js/5-arrays.js
@@ -21,3 +21,33 @@ printToScreen.innerHTML = colors.join(" <br /> ");
 
 colors.pop();
 printToScreen.innerHTML = colors.join(" <br /> ");
+
+colors.push("purple");
+printToScreen.innerHTML = colors.join(" <br /> ");
+
+colors.unshift("yellow");
+printToScreen.innerHTML = colors.join(" <br /> ");
+
+colors.shift();
+printToScreen.innerHTML = colors.join(" <br /> ");
+
+var colorGreen = colors.slice(1, 2);
+printToScreen.innerHTML = colorsGreen.join(" <br />");
+
+var colorsCopy = colors.slice(colors);
+printToScreen.innerHTML = colorsCopy.join(" <br />");
+
+printToScreen.innerHTML = colors.indexOf("green");
+
+var cars = ["audi rs6", "audi A4", "bmw e91", "Aventador", "Tesla", "Mustang"]
+for(var i = 0; i < cars.length; i++) {
+    printToScreen.innerHTML += cars[i] + "<br />";
+}
+
+
+printToScreen.innerHTML = cars.reverse() + "<br />";
+
+var countries = ["sweden", "england", "austalia", "usa", "canada"];
+countries.forEach(function(country) {
+    printToScreen.innerHTML = country + " ";
+});
